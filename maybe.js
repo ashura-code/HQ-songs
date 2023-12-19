452c96f6e5ec5b8bf79ba1fb6c0ccdf788fe35d2
adhitya nantish <adhitya2k3@gmail.com>
2023-12-19 22:46:39 +0530

    Updated acc to azure

--- maybe.js
+++ maybe.js
@@ -1,21 +1,15 @@
-const main = require("./simple");
-const express = require("express");
+const main = require('./simple');
+const express = require('express');
 const app = express();
-const PORT = 3000;
 
+app.get('/songs/:name', async (req, res) => {
+  const name = req.params.name;
+  const result = await main(name);
+  res.json(result);
+});
 
-app.get("/songs/:name",async (req,res)=>{ 
-     const name = req.params.name;
-     const result = await main(name);
-     res.json(result)
-})
+app.listen(process.env.PORT || 80);
 
-app.listen(PORT,()=>{
-     console.log(`listening on port ${PORT}`)
-})
-
-
-
-
-
-
+// app.listen(PORT,()=>{
+//      console.log(`listening on port ${PORT}`)
+// })
